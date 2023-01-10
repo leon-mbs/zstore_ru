2cff3c887b0f8b00cf7f3cb8c2ad75d4b81ffd04
leon.mbs@gmail.com <leon.mbs@gmail.com>
2023-01-10 16:39:03 +0200

    fix

--- www/config/config.php
+++ www/config/config.php
@@ -11,6 +11,16 @@ $_config['db']['name'] = 'zstore'  ;
 $_config['db']['user'] = 'root'  ;
 $_config['db']['pass'] = 'root'  ;
 
+/*
+//пример  для  PosgreSQl
+$_config['db']['host'] = 'localhost'  ;
+$_config['db']['name'] = 'test1'  ;
+$_config['db']['user'] = 'postgres'  ;
+$_config['db']['pass'] = 'root'  ; 
+$_config['db']['driver'] = 'postgres'  ; 
+*/
+
+
 $_config['smtp']['usesmtp'] = false ; //если false использктся sendmail. Заполняется  только  поле user, с ящика  которого отсылает sendmail
 $_config['smtp']['host'] = 'smtp.google.com' ;
 $_config['smtp']['port'] = 587 ;
@@ -19,12 +29,6 @@ $_config['smtp']['emailfrom'] = 'admin.google.com' ;
 $_config['smtp']['pass'] = 'пароль' ;
 $_config['smtp']['tls'] = true ;
 
-//пример  для  PosgreSQl
-$_config['db_example']['host'] = 'localhost'  ;
-$_config['db_example']['name'] = 'test1'  ;
-$_config['db_example']['user'] = 'postgres'  ;
-$_config['db_example']['pass'] = 'root'  ; 
-$_config['db_example']['padriverss'] = 'postgres'  ; 
  
   
  
